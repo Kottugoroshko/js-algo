4633f704b1d1b1e7b4c01dc27abb9adb0561c7cb
Kottugoroshko <48978725+Kottugoroshko@users.noreply.github.com>
2019-06-09 12:53:05 +0300

    changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 function writealgodemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
-    document.write("side a=" + side_a + "side b=" + side_b);
-    document.write("Площадь квадрата" + side_a * side_a);
-    document.write("Обьем куба" + side_a * side_a * side_a);
-    document.write("Площадь прямоугольника" + side_a * side_b);
+    document.write("<p>side a=" + side_a + "</p><p>side b=" + side_b + "</p>");
+    document.write("<p>Площадь квадрата" + side_a * side_a + "</p>");
+    document.write("<p>Обьем куба" + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площадь прямоугольника" + side_a * side_b + "</p>");
+}
+
+function writealgodemo2() {
+
 }
