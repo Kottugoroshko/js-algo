b4b116ae7039e4e3d96553a639154c7589f9de09
Kottugoroshko <48978725+Kottugoroshko@users.noreply.github.com>
2019-06-09 13:19:09 +0300

    Added algo for triangle

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ function writealgodemo1() {
 }
 
 function writealgodemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>side a=" + side_a + "</p><p>side b=" + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треугольника" + side_a * side_b / 2 + "</p>");
+}
+
+function writealgodemo3() {
+    var radius = Math.random() * 10 + 5;
+    var square = Math.PI * radius * radius;
 
 }
